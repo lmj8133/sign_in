55fedddb1049b38b558c4ca19ee1ff0d61f8aa89
mj.li <mj.li@algoltek.com>
2025-03-11 16:07:30 +0800

    :art: Add main.py And Run Script

---
+++ main.py
@@ -0,0 +1,6 @@
+from app import app, socketio
+
+if __name__ == '__main__':
+    # Running on all network interfaces on port 5000 in debug mode.
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+
---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Launch the Flask-SocketIO server in the background.
+python3 main.py &
+
+# Wait a few seconds for the server to start.
+sleep 3
+
+# Open the default web browser to the app URL.
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS
+  open http://127.0.0.1:5000
+else
+  # Linux
+  xdg-open http://127.0.0.1:5000
+fi
+
