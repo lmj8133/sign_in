b9930af95a9d7d6965d5d95829b39c9a1eb3f2ec
mj.li <mj.li@algoltek.com>
2025-03-11 20:48:30 +0800

    :bug: Fix Redundant UI Displaying

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def open_browser():
     webbrowser.open("http://127.0.0.1:3000")
 
 if __name__ == '__main__':
-    # Running on all network interfaces on port 5000 in debug mode.
     socketio.start_background_task(open_browser)
-    socketio.run(app, host='0.0.0.0', port=3000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=3000, debug=True, use_reloader=False)
 
