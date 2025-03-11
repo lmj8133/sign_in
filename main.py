168ec35f7dc874be5fc65819ff51d10cd4e7a154
Jane <jane.wu@algoltek.com.tw>
2025-03-11 17:33:31 +0800

    :fix: Modify Port For MacOS
    - For MacOS, Port 5000 is used. Change to 3000.

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from app import app, socketio
 
 if __name__ == '__main__':
     # Running on all network interfaces on port 5000 in debug mode.
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=3000, debug=True)
 
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Launch the Flask-SocketIO server in the background.
-python3 main.py &
+python3 ./main.py &
 
 # Wait a few seconds for the server to start.
 sleep 3
@@ -8,9 +8,9 @@ sleep 3
 # Open the default web browser to the app URL.
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS
-  open http://127.0.0.1:5000
+  open http://127.0.0.1:3000
 else
   # Linux
-  xdg-open http://127.0.0.1:5000
+  xdg-open http://127.0.0.1:3000
 fi
 
