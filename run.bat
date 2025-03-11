65aeaabe1026aaf70aef67d657cd93e75b2719f1
test <test@Apple-MacBook-Pro-A2338-M1.local>
2025-03-11 20:18:05 +0800

    :art: Open browser by main.py
    - By this approach we can remove scripts to open browser

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Start the Flask-SocketIO server in a new command window.
-start "" python main.py
-REM Wait 3 seconds to allow the server to start (adjust if needed)
-timeout /t 3
-REM Open the default web browser to your app's URL.
-start "" http://127.0.0.1:5000
-
